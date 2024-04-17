a1304ad58d59cc2094b00ce4b71e4508859bbfd6
baalaji03 <rps123a@gmail.com>
2024-04-17 13:40:42 -0700

    updated

---
+++ readme.md
@@ -0,0 +1,1 @@
+Displayed the count from 10 to 1 on the screen and then displayed Happy Indepedence Day using the callback function (using settimeout method).
---
+++ script.js
@@ -0,0 +1,35 @@
+setTimeout(()=>{
+    document.getElementById("container").innerHTML="<b>10<b>"
+    setTimeout(()=>{
+        document.getElementById("container").innerHTML="<b>9<b>"
+        setTimeout(()=>{
+            document.getElementById("container").innerHTML="<b>8<b>"
+            setTimeout(()=>{
+                document.getElementById("container").innerHTML="<b>7<b>"
+                setTimeout(()=>{
+                    document.getElementById("container").innerHTML="<b>6<b>"
+                    setTimeout(()=>{
+                        document.getElementById("container").innerHTML="<b>5<b>"
+                        setTimeout(()=>{
+                            document.getElementById("container").innerHTML="<b>4<b>"
+                            setTimeout(()=>{
+                                document.getElementById("container").innerHTML="<b>3<b>"
+                                setTimeout(()=>{
+                                    document.getElementById("container").innerHTML="<b>2<b>"
+                                    setTimeout(()=>{
+                                        document.getElementById("container").innerHTML="<b>1<b>"
+                                        setTimeout(()=>{
+                                            document.getElementById("container").innerHTML="<b>🤩🤩 Happy Indepedence Day 🤩🤩<b>"
+                                            
+                                        },600)
+                                    },600)
+                                },600)
+                            },600)  
+                        },600)
+                        
+                    },600)
+                },600)
+            },600)
+        },600)
+    },600)
+},600)
